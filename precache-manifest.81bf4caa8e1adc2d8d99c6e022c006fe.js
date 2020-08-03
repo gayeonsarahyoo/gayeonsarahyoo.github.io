08f8da0e8dcf0e75bdcd812928372c4b20347741
Sarah Yoo <gayeon1505@gmail.com>
2020-08-02 22:55:38 -0400

    Updates

---
+++ precache-manifest.81bf4caa8e1adc2d8d99c6e022c006fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a814a711ccd456e2562a7d9f12d56af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562b06215a79261b9802",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "58c23c1a6972faf1b64b",
+    "url": "/static/css/main.1b0c0556.chunk.css"
+  },
+  {
+    "revision": "562b06215a79261b9802",
+    "url": "/static/js/2.9c4cf3ca.chunk.js"
+  },
+  {
+    "revision": "05ab6b54c24ba0b72dfe2f9d5f202d84",
+    "url": "/static/js/2.9c4cf3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c23c1a6972faf1b64b",
+    "url": "/static/js/main.6e78dd4e.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
